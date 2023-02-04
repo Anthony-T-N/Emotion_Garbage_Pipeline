1c97f0b1d8e948dd4ad02e7dbeca165718fb6f77
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2023-02-04 16:17:49 +1100

    Update Kafka_Emotion_Garbage_Producer.cpp

--- Kafka_Emotion_Garbage_Producer.cpp
+++ Kafka_Emotion_Garbage_Producer.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
 
+int main()
+{
+
+    std::vector<std::string> primary_emotions = {"\033[93mHappy", "\033[36mSad", "\033[91mAnger", "\033[95mFear", "\033[92mDisgust", "\033[96mSurprise"};
+    int i = 0;
+    while (true)
+    {
+        i++;
+        std::cout << primary_emotions[rand() % primary_emotions.size()] + "\033[0m" << "\n";
+        if (i == 20)
+        {
+            break;
+        }
+    }
+}
+
+/**
+
+Kafka_Emotion_Garbage_Producer -> Kafka Instance -> Consumer
+
+**/
