be23b8739382b54fa79359a79a801a7a14a90440
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2023-02-05 16:30:20 +1100

    Update Kafka_Emotion_Garbage_Producer.cpp

--- Kafka_Emotion_Garbage_Producer.cpp
+++ Kafka_Emotion_Garbage_Producer.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
+#include "simdjson.h"
+
+//Lessons:
+// Mistake 1: Manually copied and pasted raw contents of "simdjson.cpp" into new file named that.
+// Mistake 2: Had both #include "simdjson.h" and #include "simdjson.cpp" causing "already defined in obj" errors.
+
+int json_creation()
+{
+    /**
+    simdjson::ondemand::parser parser;
+    auto json = simdjson::padded_string::load("sample_profile.json");
+    simdjson::ondemand::document doc = parser.iterate(json);
+    std::cout << uint64_t(doc["name"]["emotion"]) << " results." << std::endl;
+    return 1;
+    **/
+
+    simdjson::ondemand::parser parser;
+    simdjson::padded_string json = simdjson::padded_string::load("twitter.json");
+    simdjson::ondemand::document tweets = parser.iterate(json);
+    std::cout << uint64_t(tweets["search_metadata"]["count"]) << " results." << std::endl;
+
+    return 1;
+}
 
 int main()
 {
-
-    std::vector<std::string> primary_emotions = {"\033[93mHappy", "\033[36mSad", "\033[91mAnger", "\033[95mFear", "\033[92mDisgust", "\033[96mSurprise"};
+    json_creation();
+    std::vector<std::string> physical_states = { "Moving", "Idle" };
+    std::vector<std::string> primary_emotions = { "\033[93mHappy", "\033[36mSad", "\033[91mAnger", "\033[95mFear", "\033[92mDisgust", "\033[96mSurprise" };
     int i = 0;
     while (true)
     {
@@ -20,5 +46,13 @@ int main()
 /**
 
 Kafka_Emotion_Garbage_Producer -> Kafka Instance -> Consumer
+
+Sample Json
+{
+    "name": "John Doe",
+    "emotion":  "Happy"
+    "physical state": "Happy"
+    "source":
+}
 
 **/
